ed4111e51d8c16b926a54816ea5589048f01063b
Strattonshrugged <strattonshrugged@gmail.com>
2019-04-20 15:47:09 -0700

    3 files added

---
+++ test.ts
@@ -0,0 +1,6 @@
+
+
+/*
+import './src/identity.spec';
+*/
+
---
+++ test/jasmine-setup.ts
@@ -0,0 +1,12 @@
+
+
+
+
+
+
+
+
+
+import jasmineRequire from 'jasmine-core/lib/jasmine-core/jasmine.js';
+
+window.jasmineRequire = jasmineRequire;
